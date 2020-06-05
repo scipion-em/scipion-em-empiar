c26db107ad99f51d24f1b217ec3fdd6ab99282ee
Grigory Sharov <sharov.grigory@gmail.com>
2020-06-05 22:59:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='scipion-em-empiar',
-    version='1.0.3',
+    version='3.0.0',
     description='A Scipion plugin to make depositions to EMPIAR',
     long_description=long_description,
     url='https://github.com/scipion-em/scipion-em-empiar',
